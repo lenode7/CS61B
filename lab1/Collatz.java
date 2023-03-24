56895414a79618299d84244da3f5f12f9dcc7668
levi <117472736+lenode7@users.noreply.github.com>
2023-03-25 01:04:12 +0800

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -7,8 +7,7 @@ public class Collatz {
     public static int nextNumber(int n) {
         if (n % 2 == 0) {
             return n / 2;
-        }
-        else {
+        } else {
             return 3 * n + 1;
         }
     }
